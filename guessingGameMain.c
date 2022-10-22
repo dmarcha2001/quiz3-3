b8f95add7187a65926c624345e761758a306b0a5
dmarcha <dmarcha@cpp.edu>
2022-10-22 10:03:04 -0700

    init commit

---
+++ guessingGameMain.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+    // check user input for option selected from main menu
+    // option 1 play game
+    //must generate a random number from 1- max number
+    // ask user to guess number
+    // if correct end game and tell user they guessed number
+    // if not, tell them to guess again
+    // if user enter q go back to main menu
+
+    // option 2 change max number
+    // this will change a global variable known as max number, it will be set as 10 by default, and program must be able to save user input for the max number
+
+    // option 3 quiz the game
+    // will thank the user for playing and end program
+
+
+    return 0;
+}
